fa28f420989a5f76e86a620470b17dbb8f807ac2
sk <sskrishnas@gmail.com>
2018-03-12 16:53:07 -0500

    adding to docker hub using config.yml changes

--- src/main/java/com/springframeworkmaster/spring5webfluxrest/Spring5WebfluxRestApplication.java
+++ src/main/java/com/springframeworkmaster/spring5webfluxrest/Spring5WebfluxRestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5WebfluxRestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(Spring5WebfluxRestApplication.class, args);
 	}
 }
